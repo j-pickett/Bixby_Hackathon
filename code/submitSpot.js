253096844857e0e26e11f4f96f000ea777815b4d
Maljean <maljean@e1z4r6p12.42.us.org>
2019-04-10 18:09:34 -0700

    Submit survey is now submit spot

--- code/submitSpot.js
+++ code/submitSpot.js
@@ -1,6 +1,6 @@
-module.exports.function = function submitSurvey (spotName, email, comments, isSigningUp) {
+module.exports.function = function submitSpot (spotName, email, comments, isSigningUp) {
   // Simply construct an object from the inputs and return it.
-  // This is where your API call to submit a survey will be (if desired).
+  // This is where your API call to submit a Spot will be (if desired).
   return {
     spotName: spotName,
     email: email,
