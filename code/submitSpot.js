3557460eea32bc420bd276edf6cb86f19ad9e7b1
Maljean <maljean@e1z4r6p12.42.us.org>
2019-04-10 18:26:24 -0700

    Description box setup

--- code/submitSpot.js
+++ code/submitSpot.js
@@ -1,10 +1,10 @@
-module.exports.function = function submitSpot (spotName, email, comments, isSigningUp) {
+module.exports.function = function submitSpot (spotName, email, description, isSigningUp) {
   // Simply construct an object from the inputs and return it.
   // This is where your API call to submit a Spot will be (if desired).
   return {
     spotName: spotName,
     email: email,
-    comments: comments,
+    description: description,
     isSigningUp: isSigningUp
   };
 }
