fa15699a986735b4114ceb50e6113f42319d26ce
Austin Pickett <apickett@e1z4r6p10.42.us.org>
2019-04-11 15:56:18 -0700

    stuff shows up in input view now

--- code/commitSpot.js
+++ code/commitSpot.js
@@ -1,6 +1,7 @@
-module.exports.function = function CommitSpot (spotName, category, description, isSigningUp) {
+module.exports.function = function CommitSpot (spotName, category, description) {
   // Simply construct an object from the inputs and return it.
   // This is where your API call to submit a Spot will be (if desired).
+  
   return {
     spotName: spotName,
     category: category,
--- code/submitSpot.js
+++ code/submitSpot.js
@@ -1,10 +1,9 @@
-module.exports.function = function submitSpot (spotName, category, description, isSigningUp) {
+module.exports.function = function submitSpot (spotName, category, description) {
   // Simply construct an object from the inputs and return it.
   // This is where your API call to submit a Spot will be (if desired).
   return {
     spotName: spotName,
     category: category,
     description: description,
-    isSigningUp: isSigningUp
   };
 }
