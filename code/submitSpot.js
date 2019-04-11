e6d2c903d12af3fc6de5872624bede2f20445ccf
Maljean <maljean@e1z4r6p12.42.us.org>
2019-04-10 18:40:40 -0700

    Category input done

--- code/submitSpot.js
+++ code/submitSpot.js
@@ -1,9 +1,9 @@
-module.exports.function = function submitSpot (spotName, email, description, isSigningUp) {
+module.exports.function = function submitSpot (spotName, category, description, isSigningUp) {
   // Simply construct an object from the inputs and return it.
   // This is where your API call to submit a Spot will be (if desired).
   return {
     spotName: spotName,
-    email: email,
+    category: category,
     description: description,
     isSigningUp: isSigningUp
   };
