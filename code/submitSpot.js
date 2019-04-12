96d8f1eb8a2822c70e7856e82bceffeed166a21a
Maljean <maljean@e1z4r6p12.42.us.org>
2019-04-11 19:14:45 -0700

    Fixed naming of sourcepoint, it's now location

--- code/submitSpot.js
+++ code/submitSpot.js
@@ -1,10 +1,10 @@
-module.exports.function = function submitSpot (spotName, category, description, sourcePoint) {
+module.exports.function = function submitSpot (spotName, category, description, location) {
   // Simply construct an object from the inputs and return it.
   // This is where your API call to submit a Spot will be (if desired).
   return {
     spotName: spotName,
     category: category,
     description: description,
-    SourcePoint: sourcePoint
+    Location: location
   };
 }
