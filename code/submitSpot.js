ce187ee130767c114e5546ed473b93ef7ae3aad1
Maljean <maljean@e1z4r6p12.42.us.org>
2019-04-12 15:20:34 -0700

    Adding a spot is perfect, all fields are 100% functional

--- code/submitSpot.js
+++ code/submitSpot.js
@@ -3,7 +3,7 @@ module.exports.function = function submitSpot (spotName, categories, description
   // This is where your API call to submit a Spot will be (if desired).
   return {
     spotName: spotName,
-    categories: categores,
+    categories: categories,
     description: description,
     Location: location
   };
